bccc7ca333c00480c51536940dd097edc733525d
nkatz01 <nkatz01@dcs.bbk.ac.uk>
2020-06-02 14:47:21 +0100

    Answered question 4

--- MockOne/Tests/UnitTest1.cs
+++ MockOne/Tests/UnitTest1.cs
@@ -3,10 +3,7 @@ using static QuestionOne.Program;
 using static Alert.AlertService;
 using Alert;
 using System;
-using System.Threading;
-using System.Collections.Generic;
-using System.Linq;
-
+ 
 namespace Tests
 {
     [TestClass]
@@ -29,15 +26,10 @@ namespace Tests
             strg1.AddAlert(new DateTime());
             strg2.Add(strg1.GetKeyAtPos(0), strg1.GetValAtPos(0).AddMilliseconds(1));
            
-            //Dictionary<int, string> dict1 = new Dictionary<int, string>{ { 1, "baba" },
-            //    {2, "bibi" } };
          
-            //Dictionary<int, string> dict2 = new Dictionary<int, string> {
-            //      {2, "bibi" }, { 1, "baba" } };
 
               Assert.AreEqual(false,strg1.Equals(strg2));
-           // Assert.AreEqual(true, Enumerable.SequenceEqual(dict1, dict2));
-            //   Assert.AreEqual(true, Enumerable.SequenceEqual(dict1.OrderBy(e => e.Key), dict2.OrderBy(e => e.Key)));
+           
 
 
         }
@@ -72,9 +64,7 @@ namespace Tests
             Assert.AreEqual(false, strg1.GetHashCode() == strg2.GetHashCode(), $"{ strg1.GetHashCode()} ,{strg2.GetHashCode()}");
 
 
-            // Assert.AreEqual(true, Enumerable.SequenceEqual(dict1, dict2));
-            //   Assert.AreEqual(true, Enumerable.SequenceEqual(dict1.OrderBy(e => e.Key), dict2.OrderBy(e => e.Key)));
-
+           
 
         }
 
@@ -90,20 +80,9 @@ namespace Tests
             strg2.Add(strg1.GetKeyAtPos(1), strg1.GetValAtPos(1));
             strg2.Add(strg1.GetKeyAtPos(0), strg1.GetValAtPos(0));
 
-            //Dictionary<int, string> dict1 = new Dictionary<int, string>{ { 1, "baba" },
-            //    {2, "bibi" } };
-
-            //Dictionary<int, string> dict2 = new Dictionary<int, string> {
-            //      {2, "bibi" }, { 1, "baba" } };
-
-            //Dictionary<int, string> dict1 = new Dictionary<int, string>{ { 1, "baba" },  };
-
-            //Dictionary<int, string> dict2 = new Dictionary<int, string> {
-            //      { 1, "baba" } };
+            
              Assert.AreEqual(true, strg1.GetHashCode() == strg2.GetHashCode(), $"{ strg1.GetHashCode()} ,{strg2.GetHashCode()}");
-          //  Assert.AreEqual(true, dict1.First().GetHashCode() == dict2.First().GetHashCode(), $"{ dict1.GetHashCode()} ,{dict2.GetHashCode()}");
-            //   Assert.AreEqual(true, Enumerable.SequenceEqual(dict1.OrderBy(e => e.Key), dict2.OrderBy(e => e.Key)));
-
+          
 
         }
 
